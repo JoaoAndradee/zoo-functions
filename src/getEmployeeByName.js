19913cce48733160524544c4d4adf3e387c2cb3c
joao <joao.dev94@gmail.com>
2022-08-08 09:45:14 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let employee;
+  if (employeeName === undefined) {
+    return {};
+  }
+  data.employees.forEach((item) => {
+    if (item.firstName === employeeName || item.lastName === employeeName) {
+      employee = item;
+    }
+  });
+  return employee;
 }
 
 module.exports = getEmployeeByName;
