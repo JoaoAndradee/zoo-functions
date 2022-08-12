c67bad06ac7ab785d0fe23a0bbecf55a688b1d0c
joao <joao.dev94@gmail.com>
2022-08-12 16:34:32 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,56 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function EmployeeResponsableForWhatSpecies(index) {
+  const speciesByEmployee = data.employees[index].responsibleFor
+    .map((item) => data.species.find((element) => element.id === item).name);
+  return speciesByEmployee;
+}
+
+function locationsOfSpecies(index) {
+  const teste = EmployeeResponsableForWhatSpecies(index)
+    .map((item) => data.species.find((e) => e.name === item).location);
+  return teste;
+}
+
+function allCollaboratingPeople() {
+  return data.employees.map((item, index) => (
+    {
+      id: item.id,
+      fullName: `${item.firstName} ${item.lastName}`,
+      species: EmployeeResponsableForWhatSpecies(index),
+      locations: locationsOfSpecies(index),
+    }
+  ));
+}
+
+function findId(obj) {
+  if (allCollaboratingPeople().find((e) => e.id === obj.id)) {
+    return allCollaboratingPeople().find((e) => e.id === obj.id);
+  }
+  throw new Error('Informações inválidas');
+}
+
+function findName(obj) {
+  let employee;
+  allCollaboratingPeople().forEach((item, index) => {
+    const firstNameAndLastName = item.fullName.split(' ');
+    if (firstNameAndLastName.find((e) => e === obj.name)) {
+      employee = allCollaboratingPeople()[index];
+    }
+  });
+  return employee;
+}
+
+function getEmployeesCoverage(obj) {
+  if (obj === undefined || Object.keys(obj).length === 0) {
+    return allCollaboratingPeople();
+  }
+  if (Object.keys(obj).includes('id')) {
+    return findId(obj);
+  }
+  if (Object.keys(obj).includes('name')) {
+    return findName(obj);
+  }
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
   const firstAnimalResponsibleFor = data.employees.find((e) => e.id === id).responsibleFor[0];
   const getOlderAnimal = data.species.find((e) => e.id === firstAnimalResponsibleFor).residents
     .reduce((acc, currentValue) => {
