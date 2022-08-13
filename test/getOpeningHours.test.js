b0a6fb592904f5ba7b420231b4348c474b873b69
joao <joao.dev94@gmail.com>
2022-08-12 22:51:42 -0300

    Refatoração requisitos 6, 7, 15. Requisitos feitos: 12, 13 e 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,43 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se retorna todos os horarios ao não passar parametros', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(actual).toMatchObject(expected);
+  });
+
+  it('Verifica se os argumentos Monday e 09:00-AM retorna que o zoologico esta fechado', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toBe(expected);
+  });
+
+  it('Verifica se os argumentos Tuesday e 09:00-AM retorna que o zoologico esta aberto', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toBe(expected);
+  });
+
+  it('Verifica se os argumentos Wednesday e 09:00-PM retorna que o zoologico esta fechado', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+    expect(actual).toBe(expected);
+  });
+
+  it('Verifica se ao passar parametros inválidos é retornado um erro', () => {
+    expect(() => getOpeningHours('lalaland')).toThrowError('The day must be valid. Example: Monday');
+    expect(() => getOpeningHours('Saturday', '13:00-PM')).toThrowError('The hour must be between 0 and 12');
+    expect(() => getOpeningHours('Saturday', '-PM')).toThrowError('The hour should represent a number');
+    expect(() => getOpeningHours('Wednesday', '10:56-DM')).toThrow();
+    expect(() => getOpeningHours('Saturday', '10:105-PM')).toThrow();
+  });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,62 +1,6 @@
-const data = require('../data/zoo_data');
-const { handlerElephants, getElephants, averageAge, computeData } = require('../src/handlerElephants');
+const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('Verifica se getElephantes é uma função', () => {
-    const actual = typeof getElephants;
-    const expected = 'function';
-    expect(actual).toBe(expected);
-  });
-
-  it('Verifica se getElephants retorna o objeto elephant', () => {
-    const actual = getElephants();
-    const expected = {
-      id: 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-      name: 'elephants',
-      popularity: 5,
-      location: 'NW',
-      availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
-      residents: [
-        { name: 'Ilana', sex: 'female', age: 11 },
-        { name: 'Orval', sex: 'male', age: 15 },
-        { name: 'Bea', sex: 'female', age: 12 },
-        { name: 'Jefferson', sex: 'male', age: 4 },
-      ],
-    };
-    expect(actual).toMatchObject(expected);
-  });
-
-  it('Verifica se averageAge retorna a media de idade dos elefantes', () => {
-    const actual = averageAge(data.species[7]);
-    const expected = 10.5;
-    expect(actual).toEqual(expected);
-  });
-
-  it('Verifica se computeData retorna o esperado nos switchs', () => {
-    {
-      const actual = computeData('count', data.species[7]);
-      const expected = 4;
-      expect(actual).toBe(expected);
-    }
-
-    {
-      const actual = computeData('names', data.species[7]);
-      const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
-      expect(actual).toEqual(expected);
-    }
-
-    {
-      const actual = computeData('averageAge', data.species[7]);
-      const expected = 10.5;
-      expect(actual).toBe(expected);
-    }
-
-    {
-      const actual = computeData('lalalala', data.species[7]);
-      expect(actual).toBeNull();
-    }
-  });
-
   it('Verifica a funcao handlerElephants tem os retornos esperados', () => {
     {
       const actual = handlerElephants();
@@ -79,6 +23,23 @@ describe('Testes da função HandlerElephants', () => {
       const actual = handlerElephants('count');
       const expected = 4;
       expect(actual).toBe(expected);
+    }
+
+    {
+      const actual = handlerElephants('sada');
+      expect(actual).toBeNull();
+    }
+
+    {
+      const actual = handlerElephants('averageAge');
+      const expected = 10.5;
+      expect(actual).toEqual(expected);
+    }
+
+    {
+      const actual = handlerElephants('names');
+      const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+      expect(actual).toEqual(expected);
     }
   });
 });
